df0c805fef762be94d8fc7e3bb8da04608e6f6f9
gtsimis <gt2186@gmail.com>
2021-08-17 11:34:39 -0400

    Finished Hangman Project

--- hangman_project/lib/hangman.rb
+++ hangman_project/lib/hangman.rb
@@ -15,8 +15,7 @@ class Hangman
   end
 
   def already_attempted?(char)
-    return true if attempted_chars.include?(char)
-    false
+    attempted_chars.include?(char)
   end
 
   def get_matching_indices(char)
@@ -27,6 +26,50 @@ class Hangman
     indices.each { |i| @guess_word[i] = char  }
   end
 
-  
+  def try_guess(char)
+    if already_attempted?(char)
+      puts "that has already been attempted"
+      return false
+    end
+
+    @attempted_chars << char
+
+    if get_matching_indices(char).empty?
+      @remaining_incorrect_guesses -= 1
+    else
+      fill_indices(char, get_matching_indices(char))
+    end
+    true
+  end
+
+  def ask_user_for_guess
+    print "Enter a char: "
+    guess = gets.chomp
+    try_guess(guess)
+  end
+
+  def win?
+    if @guess_word.join("") == @secret_word
+      puts "WIN"
+      return true
+    end
+    false
+  end
+
+  def lose?
+    if @remaining_incorrect_guesses == 0
+      puts "LOSE"
+      return true
+    end
+    false
+  end
+
+  def game_over?
+    if win? || lose?
+      puts @secret_word
+      return true
+    end
+    false
+  end
 
 end
